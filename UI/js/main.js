20b4fabe4a07c26fc7d71f7c22291760f6bbf01f
iamcristos <nmereginivincent@gmail.com>
2019-01-26 19:41:02 +0000

    ft(ui): create user interface -implement an interface that user can interact with [Delivers #163499392]

---
+++ UI/js/main.js
@@ -0,0 +1,21 @@
+let explorebtn= document.getElementById('explore')
+
+explorebtn.addEventListener('click', ()=>{
+    location.href='userSignup.html'
+});
+
+let form= document.getElementsByClassName('form-group');
+
+
+let nav = document.getElementById('visible');
+let navFunction= ()=>{
+    let navItems= document.getElementsByClassName('right');
+    // navItems.forEach(item=>{
+    //     item.classList.toggle('show');
+    // });
+    for(var i = 0; i < navItems.length; i++){
+        navItems[i].classList.toggle('show');
+    }
+}
+
+nav.addEventListener('click', navFunction);
