b518e36c61b92803024ddf23be2c5bb699ae0cbb
iamcristos <nmereginivincent@gmail.com>
2019-02-05 07:11:13 +0000

    ch(endpoint) setting up node environment [Delivers #163710328]

--- src/controllers/partyController.js
+++ src/controllers/partyController.js
@@ -1,84 +1,105 @@
 import party from '../db/partyDb';
+import db from '../models/db'
 
 class PartyController {
   static createParty(req, res) {
-    const newParty = {
-      id: party.length + 1,
-      name: req.body.name,
-      hqAddress: req.body.hqAddress,
-      logourl: req.body.logourl,
-    };
-    party.push(newParty);
-    return res.status(201).send({
-      success: true,
-      message: 'party created succesfully',
-      party: newParty,
-    });
+    const {name, hqAddress,logourl} = req.body
+    const text = 'INSERT INTO Party(name,hqAddress,logourl) VALUES($1 ,$2 ,$3) RETURNING *';
+    const values = [name,hqAddress,logourl];
+
+    db.query(text,values).then((newParty)=>{
+        return res.status(201).send({
+          success: true,
+          message: 'party created succesfully',
+          party: newParty.rows[0],
+        });
+      }).catch((err)=>{
+        return res.status(422).send({
+          success: false,
+          message: 'party was not created',
+          err
+        })
+      })
+    
   }
 
   static getAllParties(req, res) {
-    return res.status(200).send({
+    const text = 'SELECT * from Party';
+    db.query(text).then((party)=>{ 
+      return res.status(200).send({
       success: true,
-      party
-    })
+      party : party.rows[0]
+        })
+      }).catch((err)=>{
+        return res.status(400).send({
+          success: false,
+          message: 'error'
+        })
+      })
   }
 
   static getASpecificParty(req,res) {
     const id = req.params.id
     const Id= parseInt(id)
-    for (let i=0; i< party.length; i++){
-      if (party[i].id === Id){
-         res.status(200).send({
-          success: true,
-          message: party[i]
-        });
-      } 
-    }
+    const text= `SELECT * FROM Party WHERE id = ${Id} `;
+
+    db.query(text).then((party)=>{
+      return+- res.status(200).send({
+        success: true,
+        message: party.rows[0]
+      });
+    }).catch((err)=>{
       return res.status(404).send({
         success: false,
         message: 'Political party dont exist'
       });
-  }
-
-  static editAParty(req,res){
-    const id = parseInt(req.params.id);
-    const {name} = req.body
-    for (let i=0; i<party.length; i++){
-      if (party[i].id === id ) {
-        party[i].name = name
-        return res.status(200).send({
-          success: true,
-          message: party[i]
-        })
-      } 
-    }
-    return res.status(404).send({
-      success: false,
-      message:"party dont exist"
     })
   }
 
-  static deleteAParty(req,res){
+  static editAParty(req,res){
+    const id = (req.params.id);
+    const Id = parseInt(id)
+    const name = req.body.name
+    const text = `UPDATE Party SET name = $1 WHERE id = $2 RETURNING * ` 
+    const values = [name , Id]
+    console.log(typeof(name))
+    
+    db.query(text,values).then((party)=>{
+      return res.status(200).send({
+        success: true,
+        message: party.rows[0]
+      })
+    }).catch((err)=>{
+      console.log(err)
+      return res.status(404).send({
+        success: false,
+        message:"party dont exist"
+      })
+    })
+        
+      } 
+    
+    
+ static deleteAParty(req,res){
     const id = parseInt(req.params.id);
-    try {
-      party.find((item)=>{
-        if (item.id === id) {
-          party.splice(id-1, 1)
-          return res.status(200).send({
-            success:true,
-            message:'Party succesfully deleted',
-            party: item
-          })
-        } 
-      }) 
-      }   catch (error) {
-          return res.status(404).send({
+      const text = `DELETE FROM Party  WHERE id = $1 RETURNING * `
+      const values = [id]
+      db.query(text,values).then((party)=>{
+        return res.status(200).send({
+          success:true,
+          message:'Party succesfully deleted',
+          party: party.rows[0]
+        })
+      }).catch((err)=>{
+        console.log(err)
+        return res.status(404).send({
           success: false,
           message: 'Party dont exist'
+      })   
       })
     }
   };
-}
+
 
 export default PartyController
 
--- src/db/partyDb.js
+++ src/db/partyDb.js
@@ -11,4 +11,16 @@ const party = [
       logoUrl: 'link',
     }];
 
+import {Pool} from 'pg' ;
+import dotenv from 'dotenv';
+
+dotenv.config()
+
+let connection = {
+  connectionString: process.env.database 
+}
+
+
+const db = new Pool(connection);
+
 export default party
