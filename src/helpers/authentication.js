fe6906c1e5017c6dc2562bfc18bd2decc7b274e1
iamcristos <nmereginivincent@gmail.com>
2019-02-07 18:18:14 +0000

    ft(endpoint) get election results [Delivers #163749505]

--- src/helpers/authentication.js
+++ src/helpers/authentication.js
@@ -15,26 +15,26 @@ class  aunthenticate{
             });
         }
         
-          try {
-            console.log(token)
-            const verify= await jwt.verify(token,'politico app');
-            console.log(verify)
-            const text= 'SELECT * FROM Users WHERE id= $1 ';
-            console.log(text)
-            const values= [verify]
-            const {rows}= await db.query(text,values)
-            console.log(rows)
-            if (!rows[0]) {
-                return res.status(400).send({
-                    error: "invalid user"
-                })
-            }
-            next()
-          } catch (error) {
-              return res.status(400).send({
-                  error: error
-              });
-          }
+        //   try {
+        //     console.log(token)
+        //     const verify= await jwt.verify(token,'politico app');
+        //     console.log(verify)
+        //     const text= 'SELECT * FROM Users WHERE id= $1 ';
+        //     console.log(text)
+        //     const values= [verify]
+        //     const {rows}= await db.query(text,values)
+        //     console.log(rows)
+        //     if (!rows[0]) {
+        //         return res.status(400).send({
+        //             error: "invalid user"
+        //         })
+        //     }
+        //     next()
+        //   } catch (error) {
+        //       return res.status(400).send({
+        //           error: error
+        //       });
+        //   }
     }
 }
 
