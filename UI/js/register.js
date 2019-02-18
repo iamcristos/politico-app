a80947cb31285cb4957df75ca590cc670fdeb8d7
iamcristos <nmereginivincent@gmail.com>
2019-02-18 20:05:14 +0000

    feat(endpoint): consume signup endpoint used fetch to consume signup api endpoint [Delivers #164027579]

--- UI/js/register.js
+++ UI/js/register.js
@@ -1,27 +1,58 @@
-let form= document.getElementById('form-group')
-
-form.addEventListener('submit', (e)=>{
-    let email= form.elements['email'];
-    let password= form.elements['password']
-    let password2= form.elements['password2']
-    let errorMsg= document.getElementById('error');
-    if (email.value.length<1) {
-        errorMsg.textContent='required';
-        errorMsg.style.color= 'red'
-        email.focus();
-    } else if (password.value.length<1) {
-        errorMsg.textContent='required';
-        errorMsg.style.color= 'red'
-        password.focus();
-    } else if (password.value.length<1) {
-        errorMsg.textContent='required';
-        errorMsg.style.color= 'red'
-        password2.focus();
-    }
-
-    else{
-        location.href='userLogin.html'
-    }
+const form= document.getElementById('form-group');
+form.addEventListener('submit', (e) => {
     e.preventDefault()
-});
+    const firstname = form.elements['firstname'].value.trim();
+    const lastname = form.elements['lastname'].value.trim();
+    const othername = form.elements['othername'].value.trim();
+    const email = form.elements['email'].value;
+    const passportUrl = form.elements['passportUrl'].value.trim();
+    const phoneNumber = form.elements['phone'].value;
+    let password= form.elements['password'].value
+    let password2= form.elements['password2'].value
+    const errMsg = []
+    try {
+        if (firstname === '')  new Error('firstname required');
+        if (lastname === '')  new Error('lastname is required');
 
+    } catch (error) {
+        console.log(errMsg)
+        console.log(error)
+        for (let i=0; i<error.length; i++){
+            errMsg.push(error[i])
+        }
+        console.log(errMsg)
+    }
+    // fetching api
+    const data ={firstname,lastname,othername,email,passportUrl,phoneNumber,password,password2}
+    const url = 'https://politicoapplication.herokuapp.com/api/v1/auth/signup';
+    const proxyurl = "https://cors-anywhere.herokuapp.com/";
+    const fetchMethod = {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            'Accept': "application/json",
+            'Content-Type': "application/json",
+            'Access-Control-Allow-Origin': "http://127.0.0.1:5500"
+    }
+}
+    fetch(proxyurl + url,fetchMethod)
+        .then((res)=> res.json())
+        .then((data)=>{            
+            if (data.status === 400) {
+                let message = data.message
+                const ul = document.getElementById('msgErr');
+                message.map((msg)=>{
+                //    let li = `<li> ${msg}</li>`;
+                let li = document.createElement('li')
+                li.innerText = msg
+                   ul.append(li)
+                });
+             const div = document.getElementById('error')   
+                
+            } else{
+                alert('registration successfull')
+                location.href = 'userLogin.html'
+            }
+        })
+        .catch((err)=> console.log(err))
+});
