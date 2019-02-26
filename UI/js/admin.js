11ffc53e85205d4d2967017290327a61eb6f4bec
iamcristos <nmereginivincent@gmail.com>
2019-02-26 13:33:19 +0000

    ft(ui): edit party -consumes edit party api endpoint [Delivers #164151792]

--- UI/js/admin.js
+++ UI/js/admin.js
@@ -26,7 +26,7 @@ fetch(url,fetchMethod)
                 div.innerHTML += `<div><h3>NAME: ${party.name}</h3> <br>
                 <h3>ADRESS: ${party.hqaddress}</h3> <br>
                 <h3>Logourl: ${party.logourl}</h3> <br>
-                <a class="grid" href="#">EDIT</a>
+                <a class="grid" id=${party.id} href="#">EDIT</a>
                 <button class="deleteGridBtn" id=${party.id} href="#">DELETE</button>
                 </div> ` 
                
@@ -65,5 +65,50 @@ const btn = document.getElementsByTagName("button")
 
 }   
 },false );
+
+// Edit political party
+document.addEventListener('click', (e)=>{
+    if (e.target.classList.contains('grid')) {
+        console.log('hello Edit')
+        const id= event.target.id
+        const form = document.getElementById('edit')
+        console.log(form)
+        form.classList.toggle('form');
+        // edit
+        console.log(id)
+        form.addEventListener('submit', (e)=>{
+            e.preventDefault()
+            console.log(id)
+            const url =  `https://politicoapplication.herokuapp.com/api/v1/parties/${id}`
+            const name = form.elements['party'].value.trim()
+            console.log(name)
+            const body = {name}
+            console.log(body)
+            console.log(url)
+            const fetchMethod = {
+            method: 'PATCH',
+            headers: {
+                'x-access-token': adminToken,
+                'Accept': "application/json",
+                'Content-Type': "application/json"
+            },
+            body: JSON.stringify(body)
+        };
+        // fetch request
+        fetch(url,fetchMethod)
+            .then((res)=>res.json())
+            .then((party)=>{console.log(party)
+            if (party.status===200) {
+                alert(`${party.message[0].name} changed successfully`)
+                location.href = 'admin.html' 
+            } else if(party.status===404) {
+                alert('Party already exist')
+                location.href = 'admin.html'
+            }
+            })
+        })
+        
+    }
+}, false)
    
    
