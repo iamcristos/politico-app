37a36960336a9d7d0b0bed300ac19e32e823e5a2
iamcristos <nmereginivincent@gmail.com>
2019-03-03 16:32:30 +0000

    ft(ui): get all candidate per office -consume get candidate api with fetch [Delivers #164351269]

--- UI/js/userOffice.js
+++ UI/js/userOffice.js
@@ -15,13 +15,13 @@ if (!confirm) {
 fetch(url,fetchMethod)
     .then((res)=>res.json())
     .then((data)=>{
-        console.log(data)
         if (data.status === 200){
             const offices = data.office
             offices.forEach((office)=>{
-                console.log(office)
                 const div = document.getElementById('officeDiv');
-                div.innerHTML += `<div class='vote'><a id=${office.id} href='#'>${office.name}</a></div>`
+                const officeName = [];
+                officeName.push(office.name)
+                div.innerHTML += `<div class='vote'><a class='officeId' id=${office.id} href='#'>${office.name}</a><form id='form'><button type='submit'>Vote</button><br></form></div>`
             })
         } else{
             const div = document.getElementById('officeDiv');
@@ -30,3 +30,56 @@ fetch(url,fetchMethod)
     })
 
 // click to show candidate
+document.addEventListener('click', (e)=>{
+    if (e.target.classList.contains('officeId')){
+        console.log('u clicked a tag')
+        const form = document.getElementById('form') 
+        form.innerHTML = ''
+        const officeId = e.target.id
+        // console.log(officeId);
+        const url = 'https://politicoapplication.herokuapp.com/api/v1/office/register';
+        const fetchMethod = {
+            method: 'GET',
+            headers: {
+                'x-access-token': confirm
+            }
+        }
+        const User = []
+        const userUrl = `https://politicoapplication.herokuapp.com/api/v1/users`
+        // Fetch all users
+        fetch(userUrl)
+            .then((res)=>res.json())
+            .then((user)=>{
+                if(user.status === 200){
+                    const users = user.user;
+                    User.push(users)
+                    return User
+                }
+            })
+        const candidateInfo = []
+        fetch(url,fetchMethod)
+            .then((res)=>res.json())
+            .then((data)=>{
+                if (data.status === 200){
+                    const candidates = data.data
+                    candidates.forEach((candidate)=>{
+                        if (candidate.office == officeId){
+                            console.log(User)                   
+                            User[0].map((item)=>{
+                                if ( candidate.candidate == item.id) {
+                                    console.log(item.firstname + item.lastname)
+                                    console.log(candidate)
+                                   const div = document.getElementById('officeDiv')
+                                   const form = document.getElementById('form') 
+                                   form.innerHTML += `<input type="radio" name="${candidate.office}" id="" value=${candidate.id}> ${item.firstname} ${item.lastname} <br>` 
+                                //    
+                                }
+                               })
+                        } 
+                    })
+                    const form = document.getElementById('form') 
+                    form.classList.toggle('formvote');
+                }
+            })
+    }
+})
