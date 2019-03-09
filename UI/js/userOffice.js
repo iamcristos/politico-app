efcf69c2892fce513681392845b1e6ef6b028e5a
iamcristos <nmereginivincent@gmail.com>
2019-03-09 09:40:17 +0000

    ft(ui):user should vote -consumes vote post api endpoint [Delivers #164362249]

--- UI/js/userOffice.js
+++ UI/js/userOffice.js
@@ -37,6 +37,10 @@ document.addEventListener('click', (e)=>{
         form.innerHTML = ''
         form.innerHTML = `<button type='submit'>Vote</button><br>`
         const officeId = e.target.id
+        const success = document.getElementById('success')
+        success.innerHTML = ""
+        const err = document.getElementById('err')
+        err.innerHTML = ""
         // console.log(officeId);
         const url = 'https://politicoapplication.herokuapp.com/api/v1/office/register';
         const fetchMethod = {
@@ -71,15 +75,11 @@ document.addEventListener('click', (e)=>{
                                     console.log(item.firstname + item.lastname)
                                     console.log(candidate);
                                     const voteValue = [candidate.office, candidate.id]
+                                    console.log(voteValue)
                                     const id = sessionStorage.getItem('id')
                                    const div = document.getElementById('officeDiv')
                                    const form = document.getElementById('form') 
                                    form.innerHTML += `<input type="radio" name='office' id=${id} value=${voteValue}> ${item.firstname} ${item.lastname} <br>` 
-                                //    form.addEventListener('submit', (e)=>{
-                                //     e.preventDefault();
-                                //     const vote = form.elements[candidate.office].value
-                                //     console.log(vote)
-                                // })
                                 }
                                })
                         } 
@@ -87,13 +87,44 @@ document.addEventListener('click', (e)=>{
                     const form = document.getElementById('form') 
                     form.classList.toggle('formvote');
                 }
-            })
+            });
+        form.addEventListener('submit',(e)=>{
+            e.preventDefault()
+            const voteInput = document.querySelector('input[name=office]:checked')
+            const voterInput = voteInput.value
+            console.log(voteInput);
+            const voteUrl = 'https://politicoapplication.herokuapp.com/api/v1/votes';
+            const office = voterInput[0]
+            console.log(office)
+            const voter = voteInput.id
+            console.log(voter + "id")
+            const candidate = voterInput[2]
+            console.log(candidate)
+            const body = {office,voter,candidate}
+            const fetchVote = {
+                method: 'POST',
+                headers: {
+                    'x-access-token': confirm,
+                    'Accept': "application/json",
+                    'Content-Type': "application/json"
+                },
+                body: JSON.stringify(body)
+            };
+
+            fetch(voteUrl,fetchVote)
+                .then((res)=>res.json())
+                .then((data)=>{
+                    console.log(data)
+                    if (data.status === 200) {
+                        const success = document.getElementById('success')
+                        success.innerHTML = `<h3>Your vote is registered successfully</h3>`
+                    } else {
+                        const err = document.getElementById('err')
+                        err.innerHTML = `<h3>${data.message}</h3>`
+                    }
+                })
+        })
+        
     }
 })
 
-const form = document.getElementById('form');
-// form.addEventListener('submit', (e)=>{
-//     e.preventDefault();
-//     const vote = form.elements[1].value
-//     console.log(vote)
-// })
