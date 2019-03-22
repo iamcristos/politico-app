31b570bfe9e7ee00e0cc589636397cc828bf0258
iamcristos <nmereginivincent@gmail.com>
2019-03-22 17:06:09 +0000

    heroku update

--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -6,9 +6,9 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 cloudinary.config({
-    cloud_name : process.env.cloud_name,
-    api_key : process.env.api_key,
-    api_secret : process.env.api_secret
+    cloud_name : process.env.CLOUD_NAME,
+    api_key : process.env.API_KEY,
+    api_secret : process.env.API_SECRET
 });
 
 
